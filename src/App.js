9bed10ff17967404dccc099395c60898e685804f
alfonsofdelarosa <alfonsofdelarosa@gmail.com>
2021-12-15 19:19:23 +0100

    modify text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ya he hecho un cambio y he recargado
         </p>
         <a
           className="App-link"
